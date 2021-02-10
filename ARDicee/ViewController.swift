19b02c7aa363c8673c8fc20710448ecca701b0e4
Starchenkov <Starchenkov@live.ru>
2021-02-10 10:22:26 +0700

    Add Trash button to remove all 3D Objects

--- ARDicee/ViewController.swift
+++ ARDicee/ViewController.swift
@@ -118,6 +118,14 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
         rollAll()
     }
+    @IBAction func removeAllDice(_ sender: UIBarButtonItem) {
+        if !diceArray.isEmpty {
+            for dice in diceArray {
+                
+                dice.removeFromParentNode()
+            }
+        }
+    }
     
     func roll (dice: SCNNode) {
         
